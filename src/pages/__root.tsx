039a1d6a1161cdb73eb1a1bd8b96c51d5c8f1fa5
dkile <dkile532@naver.com>
2024-05-07 19:30:41 +0900

    env: ionic 삭제

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,11 +1,6 @@
-import "@ionic/react/css/core.css";
-
-import { setupIonicReact } from "@ionic/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren } from "react";
-
-setupIonicReact();
 
 const queryClient = new QueryClient();
 
